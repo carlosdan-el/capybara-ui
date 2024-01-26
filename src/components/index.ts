11533df662fe4fa0225889bf5eba145c474a8e13
Carlos Daniel <carlossantos_cs@outlook.com>
2024-01-25 22:59:59 -0300

    Testando importação via subpastas

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,2 @@
 export * from './badge/badge';
 export * from './button/button';
-export * from './checkbox/checkbox';
-export * from './number-input/number-input';
-export * from './table/table';
-export * from './text-input/text-input';
-export * from './toast/toast';
