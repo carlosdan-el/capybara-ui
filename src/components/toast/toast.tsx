3d9b8935ec7fb23b53818b18c99b36e75aded6ed
Carlos Daniel <carlossantos_cs@outlook.com>
2024-03-15 19:05:36 -0300

    Otimizações no componente de Toast

--- src/components/toast/toast.tsx
+++ src/components/toast/toast.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import Lottie from 'react-lottie-player';
 import infoAnimation from './info-animation.json';
 import successAnitation from './success-animation.json';
@@ -10,7 +10,7 @@ export interface ToastProps {
     message: string
 }
 
-const animations = {
+const ANIMATIONS = {
     info: infoAnimation,
     success: successAnitation,
     warning: warningAnimation,
@@ -18,17 +18,19 @@ const animations = {
 };
 
 export const Toast: FC<ToastProps> = ({ type = 'info', message = '' }) => {
+    const animationData = useMemo(() => {
+        return ANIMATIONS[type];
+    }, [type]);
+
     return (
         <div className="flex items-center w-full max-w-sm p-4 rtl:space-x-reverse text-gray-500 bg-white rounded-lg shadow-lg" role="alert">
-            <div>
-                <Lottie
-                    loop
-                    animationData={animations[type]}
-                    play
-                    style={{ width: 28, height: 28 }}
-                    rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }}
-                />
-            </div>
+            <Lottie
+                loop
+                animationData={animationData}
+                play
+                style={{ width: 28, height: 28 }}
+                rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }}
+            />
             <div className="ps-4 text-sm font-normal">{message}</div>
         </div>
     );
